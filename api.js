e0bb94658ed978ab9c8436d0fb9cf30102ecab3c
Elizandra Krier <seuemail@exemplo.com>
2024-09-04 23:37:15 -0300

    mudança de cores

--- api.js
+++ api.js
@@ -1,18 +1,25 @@
 //api.vagalume.com.br/docs/letras/
 //chave vagalume: e0a07830773487088caa94342f94f1b5
 
-const apiKey = 'e0a07830773487088caa94342f94f1b5'
-const musicName = 'nome_da_musica'
+// const apiKey = 'e0a07830773487088caa94342f94f1b5'
+// const musicName = 'Minha Letra API'
 
-fetch(
-  `https://api.vagalume.com.br/search.php?art=${musicName}&apikey=${apiKey}`
-)
-  .then(response => response.json())
-  .then(data => {
-    const letra = data.mus[0].letras[0].letra
-    const imagem = data.mus[0].art.img
-    // Exiba a letra e a imagem na sua página
-  })
-  .catch(error => {
-    console.error('Erro ao buscar a letra:', error)
-  })
+// fetch(
+//   `https://api.vagalume.com.br/search.php?art=${musicName}&apikey=${apiKey}`
+// )
+//   .then(response => response.json())
+//   .then(data => {
+//     const letra = data.mus[0].letras[0].letra
+//     const imagem = data.mus[0].art.img
+//     // Exiba a letra e a imagem na sua página
+//   })
+//   .catch(error => {
+//     console.error('Erro ao buscar a letra:', error)
+//   })
+
+let Vagalume = require('vagalume')
+let api = Vagalume(e0a07830773487088caa94342f94f1b5)
+
+api.getNoticias().then(function (data) {
+  // faça o que quiser com o retorno
+})
