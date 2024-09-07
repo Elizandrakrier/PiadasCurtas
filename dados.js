c5b85b2e3e9594b0f20a2155e1a46984ad63d387
Elizandra Krier <seuemail@exemplo.com>
2024-09-06 23:42:17 -0300

    Mudando o tipo de conteudo

--- dados.js
+++ dados.js
@@ -1,23 +1,56 @@
 let dados = [
   {
-    titulo: 'Primeira',
-    descricao:
-      'Vamos detonar essa porra É, porra! Hello, uptown Hello, midtown Hello, downtown Hello, trench town RatamahattaRatamahatta Ratamahatta Ratamahatta',
-    link: 'https://www.vagalume.com.br',
-    tags: 'sepultura detonar rock'
+    titulo: 'A piada do médico',
+    descricao: 'Qual a parte do corpo que o médico mais gosta?  - O paciente! ',
+    tags: 'medico, piada, humor'
   },
   {
-    titulo: 'Seja Feliz - Marisa Monte',
+    titulo: 'A piada do advogado',
     descricao:
-      'Seja feliz Com seu país Seja feliz Sem raiz. Seja feliz. Com seu irmão. Seja feliz. Sem razão. Tão longa a estrada. Tão longa a sina.Tão curta a vida. Tão largo o céu. Tão largo o mar. Tão curta a vida.Curta a vida',
-    link: 'https://www.vagalume.com.br/marisa-monte/seja-feliz.html',
-    tags: 'feliz marisa monte musica'
+      'O que um advogado faz quando o cliente não paga?  - Faz uma nova lei! ⚖️',
+    tags: 'advogado, piada, humor'
   },
   {
-    titulo: 'Noite Feliz - Canções de Natal',
+    titulo: 'A piada do engenheiro',
     descricao:
-      ' Noite feliz! Noite feliz! Oh, Senhor, Deus do amor. Pobrezinho nasceu em Belém. Eis na Lapa Jesus nosso bem. Dorme em paz, oh, Jesus.Dorme em paz, oh, Jesus. Noite feliz! Noite feliz! Oh, Jesus, Deus da luz. Quão afável é Teu coração. Que quiseste nascer nosso irmão. E a nós todos salvar. E a nós todos salvar. Noite feliz! Noite feliz! Eis que no ar vem cantar. Aos pastores os Anjos do Céu. Anunciando a chegada de Deus. De Jesus Salvador. De Jesus Salvador',
-    link: 'https://www.vagalume.com.br/cancoes-de-natal/noite-feliz.html',
-    tags: 'Jesus natal noite feliz'
+      'Por que os engenheiros civis construíram a arca de Noé?  - Porque Deus não tinha os projetos! ‍♂️',
+    tags: 'engenheiro, piada, humor'
+  },
+  {
+    titulo: 'A piada do matemático',
+    descricao:
+      'O que um matemático faz quando vê duas pessoas se beijando?  - Um par ordenado! ',
+    tags: 'matematico, piada, humor'
+  },
+  {
+    titulo: 'A piada do professor',
+    descricao: 'Qual a parte favorita de um professor?  - As férias! ',
+    tags: 'professor, piada, humor'
+  },
+  {
+    titulo: 'A piada do cozinheiro',
+    descricao: 'O que o ovo disse para a frigideira?  - Quebra tudo pra mim! ',
+    tags: 'cozinheiro, piada, humor'
+  },
+  {
+    titulo: 'A piada do músico',
+    descricao: 'Qual a nota musical que mais gosta de matemática?  - Do! ',
+    tags: 'musico, piada, humor'
+  },
+  {
+    titulo: 'A piada do motorista',
+    descricao: 'O que o semáforo disse para o carro?  - Pare! ',
+    tags: 'motorista, piada, humor'
+  },
+  {
+    titulo: 'A piada do dentista',
+    descricao: 'Qual a piada favorita do dentista?  - Uma que não tem cárie! ',
+    tags: 'dentista, piada, humor'
+  },
+  {
+    titulo: 'A piada do pescador',
+    descricao:
+      'Qual a melhor forma de pescar um elefante?  - Com um anzol muito grande e muita paciência! ',
+    tags: 'pescador, piada, humor'
   }
 ]
