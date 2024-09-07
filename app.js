06ff36f03f9f60950e53cfa0dbdbcadb74cc9be2
Elizandra Krier <seuemail@exemplo.com>
2024-09-06 23:07:59 -0300

    incluindo dados na pesquisa

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log(dados)
+function pesquisar() {
+  let section = document.getElementById('resultados-pesquisa')
+
+  let campoPesquisa = document
+    .getElementById('campo-pesquisa')
+    .value.toLowerCase()
+
+  if (campoPesquisa == '') {
+    section.innerHTML = '<p> Você precisa digitar o que procura</p>'
+    return
+  }
+
+  let resultados = ''
+  let titulo = ''
+  let descricao = ''
+  let tags = ''
+
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    if (
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      resultados += `
+          <div class="item-resultado">
+            <h2>
+              <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">
+          </div>
+  `
+    }
+  }
+
+  if (!resultados) {
+    resultados = '<p> Nada foi encontrado </p>'
+  }
+
+  section.innerHTML = resultados
+}
+
+// console.log(dados)
--- dados.js
+++ dados.js
@@ -2,16 +2,22 @@ let dados = [
   {
     titulo: 'Primeira',
     descricao:
-      'Vamos detonar essa porra É, porra! Hello, uptown Hello, midtown Hello, downtown Hello, trench town RatamahattaRatamahatta Ratamahatta Ratamahatta'
+      'Vamos detonar essa porra É, porra! Hello, uptown Hello, midtown Hello, downtown Hello, trench town RatamahattaRatamahatta Ratamahatta Ratamahatta',
+    link: 'https://www.vagalume.com.br',
+    tags: 'sepultura detonar rock'
   },
   {
-    titulo: 'Segunda',
+    titulo: 'Seja Feliz - Marisa Monte',
     descricao:
-      'Biboca, garagem, favela Biboca, garagem, favela! Fubanga, maloca,bocada Fubanga, maloca, bocada! Maloca, bocada, fubanga Maloca,bocada, fubanga! Favela, garagem, biboca Favela, garagem, biboca,porra! Um, dois, três, quatro Zé Do Caixão, Zumbi, Lampião Zé DoCaixão, Zumbi, Lampião Zé Do Caixão, Zumbi, Lampião Zé Do Caixão,'
+      'Seja feliz Com seu país Seja feliz Sem raiz. Seja feliz. Com seu irmão. Seja feliz. Sem razão. Tão longa a estrada. Tão longa a sina.Tão curta a vida. Tão largo o céu. Tão largo o mar. Tão curta a vida.Curta a vida',
+    link: 'https://www.vagalume.com.br/marisa-monte/seja-feliz.html',
+    tags: 'feliz marisa monte musica'
   },
   {
-    titulo: 'Terceira',
+    titulo: 'Noite Feliz - Canções de Natal',
     descricao:
-      ' Hello, midtown Hello, downtown Hello, trench town Ratamahatta(hello) Ratamahatta (hello) Ratamahatta (porra!) Ratamahatta (filha da puta!)'
+      ' Noite feliz! Noite feliz! Oh, Senhor, Deus do amor. Pobrezinho nasceu em Belém. Eis na Lapa Jesus nosso bem. Dorme em paz, oh, Jesus.Dorme em paz, oh, Jesus. Noite feliz! Noite feliz! Oh, Jesus, Deus da luz. Quão afável é Teu coração. Que quiseste nascer nosso irmão. E a nós todos salvar. E a nós todos salvar. Noite feliz! Noite feliz! Eis que no ar vem cantar. Aos pastores os Anjos do Céu. Anunciando a chegada de Deus. De Jesus Salvador. De Jesus Salvador',
+    link: 'https://www.vagalume.com.br/cancoes-de-natal/noite-feliz.html',
+    tags: 'Jesus natal noite feliz'
   }
 ]
